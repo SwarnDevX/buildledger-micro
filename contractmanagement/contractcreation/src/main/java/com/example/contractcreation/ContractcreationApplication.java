adfa71583f609530a5c221c9a22f96510c8a22f0
SwarnDevX <SwarnenduMaity@cognizant.com>
2026-03-18 14:45:57 +0530

    fix the issue

--- contractmanagement/contractcreation/src/main/java/com/example/contractcreation/ContractcreationApplication.java
+++ contractmanagement/contractcreation/src/main/java/com/example/contractcreation/ContractcreationApplication.java
@@ -2,12 +2,13 @@ package com.example.contractcreation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ContractcreationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContractcreationApplication.class, args);
 	}
-
 }
--- contractmanagement/contractcreation/src/main/java/com/example/contractcreation/model/Contract.java
+++ contractmanagement/contractcreation/src/main/java/com/example/contractcreation/model/Contract.java
@@ -1,12 +1,9 @@
 package com.example.contractcreation.model;
 
 import com.example.contractcreation.enums.ContractStatus;
-import com.example.contractcreation.validation.ValidEndDate;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +17,6 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ValidEndDate
 @Entity
 @Table(name = "contract")
 public class Contract {
@@ -29,27 +25,22 @@ public class Contract {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long contractId;
 
-    @NotNull(message = "Vendor ID is required")
     @Column(name = "vendor_id")
     private Long vendorId;
 
     @ManyToOne
-    @JoinColumn(name = "project_id", nullable = false)
+    @JoinColumn(name = "project_id")
     @JsonBackReference
     private Project project;
 
-    @NotNull(message = "Start date is required")
     private LocalDate startDate;
 
-    @NotNull(message = "End date is required")
     private LocalDate endDate;
 
-    @NotNull(message = "Contract value is required")
-    @DecimalMin(value = "0.0", inclusive = false, message = "Value must be greater than 0")
     private BigDecimal value;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
+    @Column(name="status")
     private ContractStatus status;
 
     @OneToMany(mappedBy = "contract", cascade = CascadeType.ALL)
